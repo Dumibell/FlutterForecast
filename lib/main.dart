079955e450c7882baf837a43093146f93f24d03b
Dumibell <choyejee14@gmail.com>
2023-12-20 18:42:14 +0900

    Fix: Splash 추가 / 화면 전환시 애니메이션 효과 적용 / 현재 위치 불러오는 기능 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_weather/models/weather_model.dart';
-import 'package:flutter_weather/screens/intro_screen.dart';
 import 'package:flutter_weather/screens/weather_screen.dart';
 import 'package:flutter_weather/services/api_services.dart';
 import 'package:geolocator/geolocator.dart';
@@ -14,13 +14,15 @@ class WeatherApp extends StatefulWidget {
 }
 
 class _WeatherAppState extends State<WeatherApp> {
-  late Future<dynamic> weather;
-  double latitude = 0; // 설정해야 할 좌표 값
-  double longitude = 0; // 설정해야 할 좌표 값
+  late Future<WeatherData?> weather;
+  double lat = 0;
+  double lon = 0;
 
   @override
   void initState() {
     super.initState();
+
+    // 데이터를 앱이 시작되자마자 받아오기
     weather = _initializeApp();
   }
 
@@ -30,17 +32,68 @@ class _WeatherAppState extends State<WeatherApp> {
       home: FutureBuilder(
         future: weather,
         builder: (context, snapshot) {
-          if (snapshot.hasData && snapshot.data != null) {
-            return WeatherScreen(weatherData: snapshot.data);
-          } else {
-            return const IntroScreen();
+          if (snapshot.hasData) {
+            //데이터가 들어오고 난 후 splash 화면 제거
+            FlutterNativeSplash.remove();
           }
+          return AnimatedSwitcher(
+            duration: const Duration(seconds: 1),
+            child: snapshot.hasData
+                ? Scaffold(
+                    body: Stack(
+                      children: [
+                        WeatherScreen(
+                          weatherData: snapshot.data!,
+                          isLoading: snapshot.connectionState ==
+                              ConnectionState.waiting,
+                        ),
+                        Positioned(
+                          left: 30,
+                          top: 70,
+                          child: SizedBox(
+                            height: 30,
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: 25,
+                                  height: 25,
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Colors.black.withOpacity(0.3),
+                                        spreadRadius: 1,
+                                        blurRadius: 7,
+                                        offset: const Offset(13, 13),
+                                      ),
+                                    ],
+                                  ),
+                                  child: IconButton(
+                                      color: Colors.white,
+                                      onPressed: () {
+                                        getCurrentLocation(); // Tab하면 현재 위치 새로 불러오기
+                                      },
+                                      icon: const Icon(
+                                        Icons.assistant_navigation,
+                                        size: 30,
+                                      )),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                : Container(),
+          );
         },
       ),
     );
   }
 
   Future<void> getCurrentLocation() async {
+    // 위치권한 받아오기
     LocationPermission permission = await Geolocator.checkPermission();
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
@@ -52,27 +105,29 @@ class _WeatherAppState extends State<WeatherApp> {
       Position position = await Geolocator.getCurrentPosition();
 
       setState(() {
-        // 현재 위치 정보 업데이트
-        latitude = position.latitude;
-        longitude = position.longitude;
-        weather = ApiService().getCurrentWeather(latitude, longitude);
+        // 위도 경도 세팅
+        lat = position.latitude;
+        lon = position.longitude;
+        //현재 위치에 따른 날씨 데이터 받아오기
+        weather = ApiService().getCurrentWeather(lat, lon);
       });
-      print("latitude: $latitude, longitude: $longitude");
     } catch (e) {
       print(e);
     }
   }
 
-  Future<WeatherData> _initializeApp() async {
+  Future<WeatherData?> _initializeApp() async {
     await dotenv.load(fileName: ".env");
-    // 여기에서 필요한 초기화 작업 및 데이터 로딩 작업 수행
 
-    // API 호출
-    getCurrentLocation();
-    return ApiService().getCurrentWeather(latitude, longitude);
+    // 현재 위치 정보 업데이트
+    await getCurrentLocation();
+
+    return weather;
   }
 }
 
 void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const WeatherApp());
 }
--- lib/screens/weather_screen.dart
+++ lib/screens/weather_screen.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_weather/models/weather_model.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
-class WeatherScreen extends StatelessWidget {
+class WeatherScreen extends StatefulWidget {
   final WeatherData weatherData;
+  final bool isLoading;
 
   const WeatherScreen({
     required this.weatherData,
+    required this.isLoading,
     Key? key,
   }) : super(key: key);
 
+  @override
+  State<WeatherScreen> createState() => _WeatherScreenState();
+}
+
+class _WeatherScreenState extends State<WeatherScreen> {
   // 섭씨 변환 함수
   String getCelsius(double temp) {
     return "${(temp - 273.15).toStringAsFixed(1)}°";
@@ -16,61 +24,87 @@ class WeatherScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
+    return AnimatedSwitcher(
+      duration: const Duration(seconds: 1),
+      child: Container(
+        // 배경이미지 전환될 때 애니메이션 적용
+        key: Key(widget.weatherData.weather[0].main),
+        decoration: BoxDecoration(
           image: DecorationImage(
               colorFilter: ColorFilter.mode(
-                  Colors.black.withOpacity(0.6), BlendMode.dstATop),
-              image: AssetImage("images/${weatherData.weather[0].main}.jpg"),
-              fit: BoxFit.cover)),
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        body: DefaultTextStyle(
-          style: const TextStyle(color: Colors.white),
-          child: Padding(
-            padding: const EdgeInsets.all(30),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Text(
-                      getCelsius(weatherData.main.temp),
-                      style: const TextStyle(
-                          fontSize: 44, fontWeight: FontWeight.w600),
-                    ),
-                    DefaultTextStyle(
-                      style: const TextStyle(fontSize: 15),
-                      child: Wrap(
-                        spacing: 5,
+                  Colors.black.withOpacity(0.1), BlendMode.srcOver),
+              image: AssetImage(
+                  "images/${widget.weatherData.weather[0].main}.jpg"),
+              fit: BoxFit.cover),
+        ),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: DefaultTextStyle(
+            style: const TextStyle(color: Colors.white),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 70, horizontal: 30),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    height: 45,
+                    child: Center(
+                        child: widget.isLoading
+                            ? LoadingAnimationWidget.staggeredDotsWave(
+                                color: Colors.white, size: 30)
+                            : Text(
+                                widget.weatherData.name,
+                                style: const TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.w500),
+                              )),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        mainAxisAlignment: MainAxisAlignment.end,
                         children: [
-                          Text("H ${getCelsius(weatherData.main.tempMax)}"),
-                          Text("L ${getCelsius(weatherData.main.tempMin)}"),
+                          Text(
+                            getCelsius(widget.weatherData.main.temp),
+                            style: const TextStyle(
+                                fontSize: 44, fontWeight: FontWeight.w600),
+                          ),
+                          DefaultTextStyle(
+                            style: const TextStyle(fontSize: 15),
+                            child: Wrap(
+                              spacing: 5,
+                              children: [
+                                Text(
+                                    "H: ${getCelsius(widget.weatherData.main.tempMax)}"),
+                                Text(
+                                    "L: ${getCelsius(widget.weatherData.main.tempMin)}"),
+                              ],
+                            ),
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                widget.weatherData.weather[0].description,
+                                style: const TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.w400),
+                              ),
+                              Image.network(
+                                "https://openweathermap.org/img/wn/${widget.weatherData.weather[0].icon}@2x.png",
+                                width: 30,
+                                height: 30,
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 30,
+                          )
                         ],
                       ),
-                    ),
-                    Row(
-                      children: [
-                        Text(
-                          weatherData.weather[0].description,
-                          style: const TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.w400),
-                        ),
-                        Image.network(
-                          "https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png",
-                          width: 30,
-                          height: 30,
-                        ),
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 100,
-                    )
-                  ],
-                ),
-              ],
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
